8f9dd18161bc9f426557a8bcd24a064815c5490d
tso5912 <BThoenen@ShelterInsurance.com>
2019-10-22 13:49:19 -0500

    Working Spring Boot WebFlux example with various SSO instances

---
+++ src/test/java/shelter/b2b/B2bApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package shelter.b2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
